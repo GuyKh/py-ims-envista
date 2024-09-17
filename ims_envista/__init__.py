c55e7f77da2c0eed650e8177979a4f539b2ccd0f
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2024-09-17 15:43:49 +0300

    fix: Create Custom Exceptions (#51)
    * fix: Create Custom Exceptions
    
    * add async_timeout dep
    
    * Better configuration of TimeoutError

--- ims_envista/__init__.py
+++ ims_envista/__init__.py
@@ -1,7 +1,14 @@
 """Module providing IMS (Israel Meteorological Service) API wrapper for Envista."""
-from .commons import IMSEnvistaError
+from .commons import (
+    ImsEnvistaApiClientAuthenticationError,
+    ImsEnvistaApiClientCommunicationError,
+    ImsEnvistaApiClientError,
+)
 from .ims_envista import IMSEnvista
 
 __all__ = [
-    "IMSEnvista", "IMSEnvistaError",
+    "IMSEnvista",
+    "ImsEnvistaApiClientError",
+    "ImsEnvistaApiClientAuthenticationError",
+    "ImsEnvistaApiClientCommunicationError"
 ]
