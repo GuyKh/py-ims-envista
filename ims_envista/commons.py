b4b8dc395f68395a03f3ebeb57df834baf0e8394
Guy Khmelnitsky <guykhmel@gmail.com>
2024-10-13 09:49:39 +0300

    fix: add debug printout on GET

--- ims_envista/commons.py
+++ ims_envista/commons.py
@@ -9,16 +9,9 @@ from typing import Any
 from uuid import UUID
 
 import async_timeout
-from aiohttp import (
-    ClientError,
-    ClientResponse,
-    ClientSession,
-    TraceRequestChunkSentParams,
-    TraceRequestEndParams,
-    TraceRequestStartParams,
-)
+from aiohttp import ClientError, ClientResponse, ClientSession
 
-logger = logging.getLogger(__name__)
+_LOGGER = logging.getLogger(__name__)
 
 
 class ImsEnvistaApiClientError(Exception):
@@ -35,23 +28,6 @@ class ImsEnvistaApiClientAuthenticationError(
     ImsEnvistaApiClientError,
 ):
     """Exception to indicate an authentication error."""
-
-
-async def on_request_start_debug(session: ClientSession, context,params: TraceRequestStartParams) -> None:  # noqa: ANN001, ARG001
-    logger.debug("HTTP %s: %s", params.method, params.url)
-
-
-async def on_request_chunk_sent_debug(
-    session: ClientSession, context, params: TraceRequestChunkSentParams  # noqa: ANN001, ARG001
-) -> None:
-    if (params.method in ("POST", "PUT")) and params.chunk:
-        logger.debug("HTTP Content %s: %s", params.method, params.chunk)
-
-
-async def on_request_end_debug(session: ClientSession, context, params: TraceRequestEndParams) -> None:  # noqa: ANN001, ARG001
-    response_bytes = await params.response.read()  # Read the raw bytes
-    logger.debug("HTTP %s Response <%s>: %s", params.method, params.response.status, response_bytes)
-
 
 def _get_headers(token: UUID | str) -> dict[str, str]:
     return {
@@ -83,6 +59,7 @@ async def get(
 
     try:
         async with async_timeout.timeout(180):
+            _LOGGER.debug("Sending GET from %s", url)
             response = await session.get(
                 url=url,
                 headers=headers
